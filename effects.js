772ffba57a4e1b02fb8c92f700f8e10b425237b5
briana gude <briana@thesistesting.com>
2020-04-01 22:48:03 -0400

    make form draggable

--- effects.js
+++ effects.js
@@ -1,10 +1,56 @@
-// rotating background gradient
-let angle = 0
+// // rotating background gradient
+// let angle = 0
 
-const background = function (){
-  angle = angle + .2
-  document.body.style.backgroundImage = "linear-gradient(" + angle + "deg, #f0f791, #19a95c"
-  requestAnimationFrame(background)
+// const background = function (){
+//   angle = angle + .2
+//   document.body.style.backgroundImage = "linear-gradient(" + angle + "deg, #f0f791, #19a95c"
+//   requestAnimationFrame(background)
+// }
+
+// background()
+
+
+// make elements draggable
+// Make the DIV element draggable:
+dragElement(document.getElementById("draggable"));
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
 }
-
-background()
